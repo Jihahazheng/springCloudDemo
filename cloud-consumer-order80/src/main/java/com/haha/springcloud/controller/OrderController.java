678454f653ea89c650cf335625b9973d11b37a3c
zhengzhendu <zdzhengzhendu@163.com>
2020-06-09 21:34:28 +0800

    自定义负载均衡算法

--- cloud-consumer-order80/src/main/java/com/haha/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/haha/springcloud/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.haha.springcloud.entities.CommonResult;
 import com.haha.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,6 +26,15 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPaymentForEntity(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (forEntity.getStatusCode().is2xxSuccessful()) {
+            return forEntity.getBody();
+        }
+        return new CommonResult<>(444, "failed");
     }
 
 }
